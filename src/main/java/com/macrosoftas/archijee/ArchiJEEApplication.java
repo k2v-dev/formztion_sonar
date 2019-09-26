5c182be10199595290b1c8bceb3192b38e18e899
k2v-dev <kssi.kouakou@gmail.com>
2019-09-26 12:09:42 +0200

    test_travis

--- src/main/java/com/macrosoftas/archijee/ArchiJEEApplication.java
+++ src/main/java/com/macrosoftas/archijee/ArchiJEEApplication.java
@@ -50,6 +50,7 @@ public class ArchiJEEApplication implements CommandLineRunner{
 		//System.out.println("admin2017  encoder = " +encodepwd);
 		//petit mofication de test
 		//petit mofication de test
+		//petit mofication de test
 		user.setPassword("$2a$10$fE7BKQcc.tesDzaptjL8luXZB6MV5rvUJ13ub5aVYKqnoPmMqYd8m");
 		user.setActive(true);
 		//Role
